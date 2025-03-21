3eb5c4ffb430d8093bc3f70fb9ebc2a6325fb44c
ramziddin <mac@MacBook-Air-Mac.local>
2025-03-21 21:53:31 +0100

    Ignoring A.php and B.php

--- A.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-echo "A.php";
-echo "here some changes";
-echo "here some both on main branch";
-echo "here some on server";
-echo "here some both on main branch";
--- B.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-echo "B.php";
-echo "Hello World!";
-echo "here some both changes";
-echo "here some both on main branch";
-?>
